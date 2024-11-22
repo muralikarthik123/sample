30a9291d5088a83997cc4306c46032f84aaed3d8
muralikarthik123 <160646378+muralikarthik123@users.noreply.github.com>
2024-11-22 11:52:51 +0530

    first commit

---
+++ src/hello.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import './hello.css'
+export default function Hello() {
+  return (
+    <div className='mobile'>
+      <div style={{display:'flex'}}>
+        <div className='names'>
+          <p>Name</p>
+          <p>Address:</p>
+          <p>Mobile:</p>
+          <p>Transport:</p>
+          <p>Vechile:</p>
+          <p>Item Code:</p>
+          <p>Avabile Stock:</p>
+          <p>Quatity:</p>
+          <p>Item Name:</p>
+          <p>Avabile Stock:</p>
+          <p>Quatity:</p>
+          <p>Item Name:</p>
+          <p>Unit Rate:</p>
+          <p>DiscAmt:</p>
+          <p>DisC-%:</p>
+          <p>Weight:</p>
+        </div>
+        <div className='inputsright'>
+          <input className='inputs' type='text' />
+          <br/>
+          <input className='inputs' type='text' />
+          <br/>
+          <input className='inputs' type='text' />
+          <br/>
+          <input className='inputs' type='text' />
+          <br/>
+          <input className='inputs' type='text' />
+          <br/>
+          <input className='inputs' type='text' />
+          <br/>
+          <input className='inputs' type='text' />
+          <br/>
+          <input className='inputs' type='text' />
+          <br/>
+          <input className='inputs' type='text' />
+          <br/>
+          <input className='inputs' type='text' />
+          <br/>
+          <input className='inputs' type='text' />
+          <br/>
+          <input className='inputs' type='text' />
+          <br/>
+          <input className='inputs' type='text' />
+          <br/>
+          <input className='inputs' type='text' />
+          <br/>
+          <input className='inputs' type='text' />
+          <br/>
+          <input className='inputs' type='text' />
+          <br/>
+        </div>
+      </div>
+    </div>
+  )
+}
